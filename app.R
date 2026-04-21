578cb317e2eb4814d4403a99fc28c18dddf21e91
SamWhippen <you@example.com>
2026-04-21 12:52:25 -0700

    App deployment

--- app.R
+++ app.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 library(bslib)
+library(rsconnect)
 
 # Load data
 heart <- read.csv("HeartDiseaseData.csv")
@@ -204,3 +205,5 @@ server <- function(input, output) {
 }
 
 shinyApp(ui = ui, server = server)
+
+rsconnect::deployApp()
